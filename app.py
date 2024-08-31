18bdc797e1de07d7e206045197da4cb0f7fb0818
Luke Brumby <lbrumby29@gmail.com>
2024-08-31 22:08:49 +1000

    Add docs

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def login_page():
             st.session_state['logged_in'] = True
             st.session_state['username'] = username
             st.success('Login successful!')
-            st.experimental_rerun()
+            st.legacy_caching.clear_cache()
         else:
             st.error('Invalid username or password')
 
